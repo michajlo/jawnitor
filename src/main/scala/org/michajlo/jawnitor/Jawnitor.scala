3146090ed118621e7433ce1f8528d59fd59be9af
Michajlo Matijkiw <michajlo.matijkiw@gmail.com>
2012-12-05 00:16:26 -0500

    Add documentation to main Jawnitor class
    Addresses Issue #2

--- src/main/scala/org/michajlo/jawnitor/Jawnitor.scala
+++ src/main/scala/org/michajlo/jawnitor/Jawnitor.scala
@@ -10,7 +10,21 @@ import org.slf4j.LoggerFactory
 import org.michajlo.jawnitor.output.Slf4jMetricOutput
 import java.util.Timer
 
-
+/**
+ * Create a Jawnitor instance.  On creation a TimerTask is created to do
+ * the dirty work of reading and recording JMX metrics. For the life of this
+ * instance (until stop is called) it will log JMX metrics defined in the file
+ * at cfgLocation to the logger at loggerName every pollIntervalMillis milliseconds.
+ * 
+ * Call stop to discontinue metric collection.
+ * 
+ * @param cfgLocation a file name, either on the filesystem or classpath, in that order,
+ *        which will be used for configuring the JMX metrics which Jawnitor will keep
+ *        track of
+ * @param loggerName the slf4j logger name to which jawnitor should append the JMX
+ *        metrics it records
+ * @param pollIntervalMillis how often to read and log metrics 
+ */
 class Jawnitor(cfgLocation: String, loggerName: String, pollIntervalMillis: Long) {
 
   val metricLogger = LoggerFactory.getLogger(loggerName)
@@ -21,6 +35,9 @@ class Jawnitor(cfgLocation: String, loggerName: String, pollIntervalMillis: Long
   val timer = new Timer
   timer.schedule(pollTask, pollIntervalMillis, pollIntervalMillis)
 
+  /**
+   * Stop this instance, cancelling the internal TimerTask used for periodic execution.
+   */
   def stop() {
     timer.cancel()
   }
